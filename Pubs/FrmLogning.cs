8ee1f33ba76a4fbc126cb057ddada91f5657979a
Uriel Gaspar Ramirez <urielgr_123@hotmail.com>
2024-05-17 20:27:28 -0600

    Proyecto 6C

---
+++ Pubs/FrmLogning.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using Pubs.Fachada;
+
+namespace Pubs
+{
+    public partial class FrmLoging : Form
+    {
+        private const string cadenaConexion = "Data Source=DESKTOP-7789AN9\\SQLEXPRESS;Initial Catalog" +
+            " = pubs; Integrated Security = True";
+        frmMenu menu;
+        frmAltaVentas venta;
+        DatosPubs datos;
+        public static string nombreUsuario;
+        public static bool EsAdministrador = false;
+        public FrmLoging()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            menu = new frmMenu();
+            venta = new frmAltaVentas();
+            DetalleUsuario detalle = new DetalleUsuario();
+            List<DetalleUsuario> ListaDetalle = null;
+            try
+            {
+                string Usuario = txtUsuario.Text;
+                string Contrasena = txtContrasena.Text;
+                int usuarioValido = 0;
+                int EsAdmin = 0;
+
+                datos = new DatosPubs();
+
+                ListaDetalle = datos.ValidaUsuario(Usuario, Contrasena, true);
+
+                if (ListaDetalle.Count > 0)
+                {
+                    foreach (DetalleUsuario obj in ListaDetalle)
+                    {
+                        nombreUsuario = Usuario;
+                        menu.Load += (a, args) =>
+                        {
+                            txtUsuario.Clear();
+                            txtContrasena.Clear();
+                        };
+                        menu.Show();
+                        this.Hide();
+
+                        if (obj.EsAdmin)
+                        {
+                            menu.ActualizarVisibilidadMenuItem(true);
+                        }
+                        else
+                        {
+                            menu.ActualizarVisibilidadMenuItem(false);
+
+                        }
+                    }
+                }
+                else
+                    MessageBox.Show("Nombre de usuario o contraseña incorrectos.");
+                
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        private void FrmLoging_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Pubs/frmAltaAutores.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pubs
+{
+    public partial class frmAltaAutores : Form
+    {
+        frmAutores autores;
+        public frmAltaAutores()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            DatosPubs datos = new DatosPubs();
+            try
+            {
+
+                datos.AltaAutores(mtxtIdAutor.Text, txtNombre.Text, txtApellido.Text, mtxtTelefono.Text,
+                    txtDireccion.Text, txtCiudad.Text, cbEstado.SelectedItem.ToString(), txtCodigoPostal.Text, chkbContrato.Checked);
+
+                MessageBox.Show("Autor agregado", "Informativo", MessageBoxButtons.OK);
+
+                //Se usa componente MaskedTextBox para dar formato al textbox se puede modificar su propiedad en "Propiedades > Mask dar en los 3 puntos y seleccionar o generar un mascarado
+                mtxtIdAutor.Clear();//MaskedTextBox
+
+                txtNombre.Clear();
+                txtApellido.Clear();
+
+                mtxtTelefono.Clear();//MaskedTextBox
+
+                txtDireccion.Clear();
+                txtCiudad.Clear();
+                cbEstado.SelectedIndex = -1;
+                txtCodigoPostal.Clear();
+                chkbContrato.Checked = false;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error al agregar autor: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            autores = new frmAutores();
+            autores.Show();
+            this.Close();
+        }
+
+        private void frmAltaAutores_Load(object sender, EventArgs e)
+        {
+            LlenarEstados();
+        }
+
+        private void LlenarEstados()
+        {
+            DataTable data = new DataTable();
+            DatosPubs datos = new DatosPubs();
+
+            data = datos.ObtenerEstados();
+
+            foreach (DataRow item in data.Rows)
+            {
+                cbEstado.Items.Add(item[0].ToString());
+            }
+        }
+    }
+}
